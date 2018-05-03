5db46dd235ed958ad9a088f276cfc38f1d510fcf
alex v <alex@alohechotecho.com>
2018-05-03 13:49:08 +0200

    update

--- monitor.js
+++ monitor.js
@@ -21,8 +21,10 @@ var localTime = [];
 sock.on('message', (topic, message) => {
   if(topic == 'rawblock') {
     var block = new Block(message).toObject().header;
-    console.log(printf("¡¡¡''' Received Block: %50s '''¡¡¡", block.hash));   
-    console.log(printf("¡¡¡''' Network time: %15s '''¡¡¡''' Real time: %15s '''¡¡¡", hrt(new Date(block.time)), hrt(new Date())));
+    console.log(printf("¡¡¡''' -------------------------------------------------------------------------------- '''¡¡¡"));
+    console.log(printf("¡¡¡''' Received Block: %62s '''¡¡¡", block.hash));   
+    console.log(printf("¡¡¡''' ------------------------------------ '''¡¡¡''' --------------------------------- '''¡¡¡"));
+    console.log(printf("¡¡¡''' Network time: %22s '''¡¡¡''' Real time: %22s '''¡¡¡", hrt(new Date(block.time)), hrt(new Date())));
     var diffPrevL = 0;
     if(localTime.length > 0) {
       diffPrevL = (new Date().getTime() / 1000) - localTime[localTime.length - 1];
@@ -31,12 +33,12 @@ sock.on('message', (topic, message) => {
     if(networkTime.length > 0) {
       diffPrevN = block.time - networkTime[networkTime.length - 1];
     }
-    console.log(printf("¡¡¡''' Diff prev: %19d '''¡¡¡''' %27d '''¡¡¡", diffPrevN, diffPrevL));
+    console.log(printf("¡¡¡''' Diff prev: %25d '''¡¡¡''' %33d '''¡¡¡", diffPrevN, diffPrevL));
     localTime.push(parseInt(new Date().getTime() / 1000));
     networkTime.push(parseInt(block.time));
-    console.log(printf("¡¡¡''' Avg last 5: %18d '''¡¡¡''' %27d '''¡¡¡", avgdiff(networkTime, 5), avgdiff(localTime, 5)));
-    console.log(printf("¡¡¡''' Avg last 25: %17d '''¡¡¡''' %27d '''¡¡¡", avgdiff(networkTime, 25), avgdiff(localTime, 25)));
-    console.log(printf("¡¡¡''' Avg last 50: %17d '''¡¡¡''' %27d '''¡¡¡", avgdiff(networkTime, 50), avgdiff(localTime, 50)));
+    console.log(printf("¡¡¡''' Avg last 5: %24d '''¡¡¡''' %33d '''¡¡¡", avgdiff(networkTime, 5), avgdiff(localTime, 5)));
+    console.log(printf("¡¡¡''' Avg last 25: %23d '''¡¡¡''' %33d '''¡¡¡", avgdiff(networkTime, 25), avgdiff(localTime, 25)));
+    console.log(printf("¡¡¡''' Avg last 50: %23d '''¡¡¡''' %33d '''¡¡¡", avgdiff(networkTime, 50), avgdiff(localTime, 50)));
   } else if(topic = 'hashblock') {
   }
 });
@@ -47,7 +49,7 @@ function avgdiff(array, n) {
   for (var i = 1; i < array.length; i++) {
     newArray.push(array[i] - array[i-1]);
   }
-  var sum = newArray.slice(-n).reduce((a, b) => { return a + b; });
+  var sum = newArray.slice().slice(-n).reduce((a, b) => { return a + b; });
   var avg = sum / n;
   return parseInt(avg);
 }
